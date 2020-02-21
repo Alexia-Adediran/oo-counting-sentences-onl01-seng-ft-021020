4e712413d84c8d49b72859b889756fbd1b7e1ed7
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-21 14:35:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,24 +1,3 @@
-# require 'pry'
-
-# class String
-
-#   def sentence?
-    
-#   end
-
-#   def question?
-
-#   end
-
-#   def exclamation?
-
-#   end
-
-#   def count_sentences
-
-#   end
-# end
-
 require 'pry'
 
 class String
